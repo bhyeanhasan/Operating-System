4fa62adbf75255cab6b81939b65c7ad80ad9a304
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-08-01 19:45:21 +0600

    as

--- Programming/Multithread.py
+++ Programming/Multithread.py
@@ -1,30 +1,21 @@
 import threading
 
 
-def one(num):
+def one(num, char):
     for i in range(num):
-        print("A")
+        print(char)
 
 
-def two(num):
-    for i in range(num):
-        print("B")
+# creating thread
+t1 = threading.Thread(target=one, args=(1000, 't1'))
+t2 = threading.Thread(target=one, args=(500, 't2'))
+t3 = threading.Thread(target=one, args=(1000, 't3'))
+
+t1.start()
+t2.start()
+t3.start()
+
+t2.join()
+print("T2 ses na houa porjonto wait koro")
 
 
-if __name__ == "__main__":
-    # creating thread
-    t1 = threading.Thread(target=one, args=(500,))
-    t2 = threading.Thread(target=two, args=(500,))
-
-    # starting thread 1
-    t1.start()
-    # starting thread 2
-    t2.start()
-
-    # wait until thread 1 is completely executed
-    t1.join()
-    # wait until thread 2 is completely executed
-    t2.join()
-
-    # both threads completely executed
-    print("Done!")
