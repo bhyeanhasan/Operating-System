e0ac66326c75e88bf3e5b94bb13638808b0c1411
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-08-07 08:34:52 +0600

    FCS AND SJF

---
+++ Mahbub Sir/1 FCFS.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+
+num_process = int(input("Number of process: "))
+
+arrival_time = []
+burst_time = []
+system_idle = 0
+waiting_time = []
+turnaround_time = []
+process_sequence = []
+time_entry = []
+time_exit = []
+
+time = 0
+
+print("Give arrival time in sequence: ")
+for i in range(num_process):
+    val = float(input())
+    arrival_time.append(val)
+
+print("Give burst time in sequence: ")
+for i in range(num_process):
+    val = float(input())
+    burst_time.append(val)
+
+for i in range(num_process):
+
+    if time < arrival_time[i]:  # system delay calculate kore nilam
+        system_idle += abs(time - arrival_time[i])
+        time = arrival_time[i]
+
+    process_sequence.append(i)
+    time_entry.append(time)
+    time = time + burst_time[i]
+    time_exit.append(time)
+
+for i in range(num_process):
+    turnaround_time.append(time_exit[i] - arrival_time[i])
+    waiting_time.append(turnaround_time[i] - burst_time[i])
+
+print("Process Sequence = ", process_sequence)
+print("Process start time ", time_entry)
+print("Process finished at ", time_exit)
+print("Turnaround time ", turnaround_time)
+print("Waiting time ", waiting_time)
+
+plt.barh(y=process_sequence, width=burst_time, left=time_entry)
+plt.show()
+
+# 5
+# 0
+# 0
+# 0
+# 0
+# 0
+# 2
+# 1
+# 8
+# 4
+# 5
---
+++ Mahbub Sir/2 SJF.py
@@ -0,0 +1,115 @@
+num_process = int(input("Number of process: "))
+
+process = []
+
+for i in range(num_process):
+    print("process :", i)
+    process_id = i
+    arrival = float(input())
+    burst = float(input())
+    entry_time = -1
+    exit_time = -1
+    in_queue = False
+    data = [process_id, arrival, burst, entry_time, exit_time, in_queue]
+    process.append(data)
+
+process.sort(key=lambda x: x[2])
+process.sort(key=lambda x: x[1])
+
+time = 0.0
+system_idle = 0
+process_queue = []
+process_schedule = []
+
+
+def complete_process(i):
+    global time, process, system_idle, process_queue, num_process
+
+    for j in range(num_process):
+        if process[j][0] == i:
+            i = j
+
+    if time < process[i][1]:
+        system_idle += abs(time - process[i][1])
+        time = process[i][1]
+
+    process_schedule.append(i)
+    process[i][3] = time
+    time = time + process[i][2]
+    process[i][4] = time
+
+    process_queue.pop(0)
+
+    for j in range(num_process):
+        if process[j][1] <= time and process[j][5] == False:
+            process[j][5] = True
+            process_queue.append(process[j])
+
+    process_queue.sort(key=lambda x: x[2])
+
+
+x = 0
+while len(process_schedule) < num_process:
+    process[x][5] = True
+    process_queue.append(process[x])
+    while len(process_queue) != 0:
+        complete_process(process_queue[0][0])
+    for i in range(num_process):
+        if not process[i][5]:
+            x = i
+            break
+
+turnaround_time = []
+waiting_time = []
+process.sort(key=lambda x: x[3])
+
+for i in range(num_process):
+    turnaround_time.append(process[i][4] - process[i][1])
+    waiting_time.append(turnaround_time[i] - process[i][2])
+    print("Process ", process[i][0], " Turn around time = ", turnaround_time[i])
+    print("Process ", process[i][0], " Waiting time = ", waiting_time[i])
+
+print("Process Execution Order = ", process_schedule)
+print("Total System Idle = ", system_idle)
+print("System Utilization = ", ((time - system_idle) / time) * 100)
+
+'''
+6
+0
+20
+25
+25
+30
+25
+60
+15
+100
+10
+105
+10
+
+3
+1
+3
+2
+3
+3
+2
+
+3
+0
+8
+.4
+4
+1
+1
+
+
+3
+0
+2
+4
+3
+5
+4
+'''
