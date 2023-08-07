cf3d644dd78e863a68a3239786522329aa7e7b03
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2023-08-07 09:05:12 +0600

    Non preemptive

--- Mahbub Sir/3 Non-Preemptive Priority.py
+++ Mahbub Sir/3 Non-Preemptive Priority.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 num_process = int(input("Number of process: "))
 
@@ -9,13 +9,14 @@ for i in range(num_process):
     process_id = i
     arrival = float(input())
     burst = float(input())
+    priority = float(input())
     entry_time = -1
     exit_time = -1
     in_queue = False
-    data = [process_id, arrival, burst, entry_time, exit_time, in_queue]
+    data = [process_id, arrival, burst, entry_time, exit_time, in_queue, priority]
     process.append(data)
 
-process.sort(key=lambda x: x[2])
+process.sort(key=lambda x: x[6], reverse=True)
 process.sort(key=lambda x: x[1])
 
 time = 0.0
@@ -47,7 +48,7 @@ def complete_process(i):
             process[j][5] = True
             process_queue.append(process[j])
 
-    process_queue.sort(key=lambda x: x[2])
+    process_queue.sort(key=lambda x: x[6], reverse=True)
 
 
 x = 0
@@ -74,46 +75,19 @@ for i in range(num_process):
 print("Process Execution Order = ", process_schedule)
 print("Total System Idle = ", system_idle)
 print("System Utilization = ", ((time - system_idle) / time) * 100)
+print(process)
 
 plt.barh(y=process_schedule, width=[i[2] for i in process], left=[i[3] for i in process])
 plt.show()
 '''
-6
-0
-20
-25
-25
-30
-25
-60
-15
-100
-10
-105
-10
-
-3
-1
-3
-2
-3
-3
-2
-
 3
 0
-8
-.4
-4
-1
-1
-
-
-3
-0
-2
-4
-3
 5
-4
+5
+1
+2
+5
+3
+3
+10
 '''
